e912360aab8542c92f14cb7b32a27a49ed7282a0
mengjujie <1991433788@qq.com>
2017-07-04 16:41:18 +0800

    CPU监控第一版练习git提交

---
+++ src/main/java/com/cmsz/test/monitorservice/IMonitorService.java
@@ -0,0 +1,7 @@
+package com.cmsz.test.monitorservice;
+
+import com.cmsz.test.bean.MonitorInfoBean;
+
+public interface IMonitorService {
+	public MonitorInfoBean getMonitorInfoBean() throws Exception; 
+}
---
+++ src/test/java/com/cmsz/test/monitorservice/impl/MonitorServiceImplTest.java
@@ -0,0 +1,30 @@
+package com.cmsz.test.monitorservice.impl;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)  
+@ContextConfiguration(locations={"classpath:spring.xml"})
+public class MonitorServiceImplTest {
+
+	@Autowired
+	MonitorServiceImpl monitorCPU;
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testDoService() throws Exception {
+		monitorCPU.doService();
+	}
+
+}
